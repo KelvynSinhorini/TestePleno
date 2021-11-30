c8905c5e593b802fd1b6752c62d50d0874e6ce40
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2021-11-29 21:23:41 -0300

    Adicionar arquivos de projeto.

---
+++ TestePleno/Controllers/FareController.cs
@@ -0,0 +1,24 @@
+﻿using TestePleno.Models;
+using TestePleno.Services;
+
+namespace TestePleno.Controllers
+{
+	public class FareController
+	{
+		private readonly OperatorService _operatorService;
+		private readonly FareService FareService;
+
+		public FareController()
+		{
+			_operatorService = new OperatorService();
+		}
+
+		public void CreateFare(Fare fare, string operatorCode)
+		{
+			var selectedOperator = _operatorService.GetOperatorByCode(operatorCode);
+			fare.OperatorId = selectedOperator.Id;
+
+			FareService.Create(fare);
+		}
+	}
+}
